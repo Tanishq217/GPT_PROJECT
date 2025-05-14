e6090495d471832387f11be79d39304e41d92ff4
Tanishq217 <tanishqsinghtanishq135@gmail.com>
2025-05-14 23:48:40 +0530

    Made MOre Changes and did another section with css style and frontend

--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
 import './whatGPT3.css'
+import {Feature} from '../../components'
 const WhatGPT3 = () => {
   return (
-    <div>whatGPT3</div>
+    <div className='gpt3__whatgpt3 section__margine' id="wgpt3">
+      <div className='gpt3__whatgpt3-feature'>
+        <Feature/>
+      </div>
+      <div className='gpt3__whatgpt3-heading'>
+        <h1 className='gradient-text'>
+          The possibilities are beyond your imagination
+        </h1>
+        <p>Explore the Library</p>
+      </div>
+      <div className='gpt3__whatgpt3-container'>
+        <Feature title='Chatbots' text='We so many things for you. We are the best in the world.'/>
+        <Feature title='Knowledgebase' text='We so many things for you. We are the best in the world.'/>
+        <Feature title='Education' text='We so many things for you. We are the best in the world.'/>
+        <Feature title='Cloud' text='We so many things for you. We are the best in the world.'/> 
+      </div>
+    </div>
   )
 }
 
