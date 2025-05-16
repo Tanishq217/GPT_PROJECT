83dcfa6af7c31fb053091801860c59bb35d24637
Tanishq217 <tanishqsinghtanishq135@gmail.com>
2025-05-16 10:25:01 +0530

    MADE THE CHANGES AND RESOLVE THE ERRORS , i also added features and whatisgpt etc

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
+// src/App.js
 import React from 'react';
 import './App.css';
-import {Footer , Blog , Possibility, Features, WhatGPT3, Header} from './containers';
-import {CTA , Brand , Navbar} from './components';
-const App = () => {
-  return (
-    <div className = "App">
-      <div className = "gradient__bg">
-        <Navbar />
-        <Header />
-      </div>
-      <Brand />
-      <WhatGPT3 />
-      <Features />
-      <Possibility />
-      <CTA />
-      <Blog />
-      <Footer />
+
+import { 
+  Header, WhatGPT3, Features, Possibility, Blog, Footer 
+} from './containers';
+import { Navbar, Brand, CTA } from './components';
+
+const App = () => (
+  <div className="App">
+    <div className="gradient__bg">
+      <Navbar />
+      <Header />
     </div>
-  );
-};
+    <Brand />
+    <WhatGPT3 />
+    <Features />
+    <Possibility />
+    <CTA />
+    <Blog />
+    <Footer />
+  </div>
+);
 
 export default App;
